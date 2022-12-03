8c7a6b25791df5592b7a48bd3b6e671215260f01
Daniele Conti <fourlastor@users.noreply.github.com>
2022-12-03 12:57:22 +0000

    Add initial preview pane with demo character

--- src/jvmMain/kotlin/io/github/fourlastor/editor/PreviewPane.kt
+++ src/jvmMain/kotlin/io/github/fourlastor/editor/PreviewPane.kt
@@ -1,17 +1,41 @@
 package io.github.fourlastor.editor
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Slider
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.res.useResource
+import androidx.compose.ui.unit.IntOffset
 
 @Composable
 fun PreviewPane(
     modifier: Modifier,
 ) {
     Box(
-        modifier = modifier.background(Color(0.5f, 0.2f, 0.2f))
+        modifier = modifier
     ) {
+        var zoom by remember { mutableStateOf(1f) }
+        val bmp = remember { useResource("player.png") { loadImageBitmap(it) } }
+        Canvas(modifier = Modifier.matchParentSize()) {
+            scale(zoom * 20) {
+                drawImage(
+                    image = bmp,
+                    dstOffset = IntOffset(center.x.toInt(), center.y.toInt()),
+                    filterQuality = FilterQuality.None
+                )
+            }
+        }
+
+        Slider(
+            value = zoom,
+            modifier = Modifier.align(Alignment.BottomEnd).fillMaxWidth(0.4f),
+            onValueChange = { zoom = it }
+        )
     }
 }
