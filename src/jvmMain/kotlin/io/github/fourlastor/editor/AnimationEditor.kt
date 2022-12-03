6ee6e6e2034a837e2f0d2c78900ff57fee7ee3c1
Daniele Conti <fourlastor@users.noreply.github.com>
2022-12-03 13:13:21 +0000

    Add panning to preview pane

--- src/jvmMain/kotlin/io/github/fourlastor/editor/AnimationEditor.kt
+++ src/jvmMain/kotlin/io/github/fourlastor/editor/AnimationEditor.kt
@@ -1,6 +1,7 @@
 package io.github.fourlastor.editor
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
@@ -8,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 @Preview
 fun AnimationEditor() {
--- src/jvmMain/kotlin/io/github/fourlastor/editor/PreviewPane.kt
+++ src/jvmMain/kotlin/io/github/fourlastor/editor/PreviewPane.kt
@@ -1,31 +1,46 @@
 package io.github.fourlastor.editor
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.PointerMatcher
+import androidx.compose.foundation.gestures.onDrag
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Slider
+import androidx.compose.material.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.graphics.drawscope.withTransform
+import androidx.compose.ui.input.pointer.PointerButton
 import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.res.useResource
 import androidx.compose.ui.unit.IntOffset
 
+@ExperimentalFoundationApi
 @Composable
 fun PreviewPane(
     modifier: Modifier,
 ) {
+    var pan by remember { mutableStateOf(Offset.Zero) }
     Box(
-        modifier = modifier
+        modifier = modifier.onDrag(matcher = PointerMatcher.mouse(PointerButton.Secondary)) {
+            pan += it
+        }
     ) {
         var zoom by remember { mutableStateOf(1f) }
+        Surface { }
         val bmp = remember { useResource("player.png") { loadImageBitmap(it) } }
         Canvas(modifier = Modifier.matchParentSize()) {
-            scale(zoom * 20) {
+            withTransform({
+                translate(pan.x, pan.y)
+                scale(zoom * 20)
+            }) {
                 drawImage(
                     image = bmp,
                     dstOffset = intCenter - bmp.center,
